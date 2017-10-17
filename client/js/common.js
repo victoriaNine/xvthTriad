559a3a91d8ceab092a3bf2dbb42b0077742bda01
Victoria Bissengué <victoria.bissengue@canal-plus.com>
2017-10-17 09:25:01 +0200

    refactor(assetManager): isolate class in separate file

--- client/js/common.js
+++ client/js/common.js
@@ -1,43 +1,13 @@
 import $ from 'jquery';
-import { clone, get, set, isNil, map, isFunction, isArray, each, isString, random, filter, find, isDate, omit } from 'lodash';
+import { clone, isNil, map, isFunction, isArray, each, isString, random, filter, find, isDate, omit } from 'lodash';
 import Backbone from 'backbone';
 import Elo from 'arpad';
 import { TimelineMax } from 'gsap';
 
+import AssetManager from 'Modules/AssetManager';
+
 import cardList from './data/cardList.json';
 import countryList from './data/countryList.json';
-
-class AssetManager {
-  constructor () {
-    this.img = {
-      ui      : {},
-      cards   : {},
-      avatars : {}
-    };
-
-    this.audio = {
-      bgm : {},
-      sfx : {}
-    };
-  }
-
-  set (path, value) { return set(this, path, value); }
-  get (path, defaultValue, noClone) {
-    const asset = get(this, path);
-
-    if (!asset) {
-      _$.debug.warn("Missing asset:", path);
-
-      if (defaultValue) {
-        return this.get(path, defaultValue, noClone);
-      }
-
-      return null;
-    }
-
-    return noClone ? asset : asset.cloneNode(true);
-  }
-}
 
 const startTime = Date.now();
 const app       = Object.create(null, {
